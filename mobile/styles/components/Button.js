8adb19a721f415e8be83695afa5aaccf4b687cdc
Jordan Chivers <jchivers@and.digital>
2020-06-23 10:49:20 +0100

    refactor(mobile): removed comments

--- mobile/styles/components/Button.js
+++ mobile/styles/components/Button.js
@@ -3,62 +3,6 @@ import { Button as PaperButton } from 'react-native-paper';
 import styled, { css } from '@emotion/native';
 import colors from '../constants/ColorScheme';
 
-// const StyledButton = styled(PaperButton)`
-//   background-color: ${(props) => (props.primary ? colors.greenDark : colors.redDark)};
-//   width: ${(props) => (props.small ? '200px' : '400px')};
-// `;
-
-// const StyledButton = styled(PaperButton)`
-//   background-color: ${(props) => (props.primary ? colors.greenDark : colors.redDark)};
-
-//   background-color: ${(props) => {
-//     switch (props.theme) {
-//       case 'primary':
-//         return colors.greenDark;
-//       case 'secondary':
-//         return colors.redDark;
-//       default:
-//         return colors.blueDark;
-//     }
-//   }};
-
-//   width: ${(props) => {
-//     switch (props.size) {
-//       case 'small':
-//         return '50px';
-//       case 'large':
-//         return '200px';
-//       default:
-//         return '500px';
-//     }
-//   }};
-// `;
-
-// const StyledButton = styled(PaperButton)`
-//   ${(props) =>
-//     props.primary &&
-//     css`
-//       background-color: ${colors.greenDark};
-//       padding: 20px;
-//     }
-//   `}
-
-//   ${(props) =>
-//     props.secondary &&
-//     css`
-//       background-color: ${colors.redDark};
-//       padding: 40px;
-//     }
-//   `}
-
-//   ${(props) =>
-//     props.small &&
-//     css`
-//       width: 40px;
-//     }
-//   `}
-// `;
-
 const StyledButton = styled(PaperButton)`
   background-color: ${(props) => {
     if (props.primary) {
