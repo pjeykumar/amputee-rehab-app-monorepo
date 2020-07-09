87efd93b43f6a20a8e89634c067c23401d2f1a74
Anita Chan <achan@and.digital>
2020-07-09 15:27:31 +0100

    Added styles

--- mobile/src/styles/components/Button.js
+++ mobile/src/styles/components/Button.js
@@ -3,20 +3,14 @@ import { Button as PaperButton } from 'react-native-paper';
 import styled from '@emotion/native';
 import { colours, font } from '../constants';
 import { Text } from './index';
+import getTypeStyle from '../../utils/typeStyles';
 
 const StyledButton = styled(PaperButton)`
   margin: 8px 0;
   justify-content: center;
+  border-width: 2px;
 
   align-self: ${(props) => (props.alignSelf !== undefined ? props.alignSelf : 'auto')};
-
-  background-color: ${(props) => {
-    if (props.primary) {
-      return colours.blueDark;
-    } else if (props.secondary) {
-      return colours.yellowDark;
-    }
-  }};
 
   width: ${(props) => {
     if (props.small) {
@@ -25,9 +19,49 @@ const StyledButton = styled(PaperButton)`
       return '200px';
     }
   }};
+
+  background-color: ${(props) => {
+    const backgroundColourConstants = {
+      primary: colours.blueDarker,
+      secondary: colours.yellow,
+      tertiary: colours.red,
+      inverted: {
+        primary: colours.yellowLighter,
+        secondary: colours.white,
+        tertiary: colours.yellowLighter,
+      },
+    };
+    return getTypeStyle(props, backgroundColourConstants);
+  }};
+
+  border-color: ${(props) => {
+    const borderColourConstants = {
+      primary: colours.blueDarker,
+      secondary: colours.yellow,
+      tertiary: colours.red,
+      inverted: {
+        primary: colours.blueDarker,
+        secondary: colours.greyLighter,
+        tertiary: colours.redDark,
+      },
+    };
+    return getTypeStyle(props, borderColourConstants);
+  }};
 `;
 
+const textColoursConstant = {
+  primary: colours.yellowLighter,
+  secondary: colours.greyDarker,
+  tertiary: colours.yellowLighter,
+  inverted: {
+    primary: colours.blueDarker,
+    secondary: colours.greyDark,
+    tertiary: colours.redDark,
+  },
+};
+
 const Button = ({ children, mode = 'contained', ...props }) => {
+  const textColour = getTypeStyle(props, textColoursConstant) || colours.yellowLighter;
   return (
     <StyledButton uppercase={false} mode={mode} {...props}>
       <Text color={colours.white} fontWeight={font.bold} fontSize={font.small}>
---
+++ mobile/src/styles/components/Row.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from '@emotion/native';
+
+const StyledRow = styled.View`
+  flex-direction: row;
+  margin: ${(props) => props.margin || '20px 0 0'};
+  justify-content: ${(props) => props.justifyContent || 'space-between'};
+`;
+
+const Row = ({ children, ...props }) => {
+  return <StyledRow {...props}>{children}</StyledRow>;
+};
+
+export default Row;
